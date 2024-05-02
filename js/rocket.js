e157314df603bb2d6844029ce2d7edb19d82be5f
Milian <miliankorkiakoski@gmail.com>
2024-05-02 12:36:45 +0300

    nyn
    ynyn

---
+++ js/rocket.js
@@ -0,0 +1,26 @@
+var timer = null;
+var CountDownNumber = 10;
+
+var changeState= function(state){
+    document.body.classname = 'body-state' + state;
+    clearInterval(timer);
+    if(state == 2) {
+        document.getElementById('countdown').innerHTML= CountDownNumber;
+        CountDownNumber = CountDownNumber -1;
+        if (countdownnumber <=0){
+            changeState(3);
+        }
+    }, 200);
+
+}
+else if (state == 3) {
+    var success = setTimeout (function(){
+        var randomNumber = math.round(math.random()*10);
+        // success
+        if (randomNumber > 5) {
+            changeState (4);
+        } else {
+            changeState(5); // oh no
+        }
+        }, 2000);
+    }
